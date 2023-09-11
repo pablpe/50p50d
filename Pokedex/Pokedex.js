65fe8409539c3776e9e0eb9954c72a8abd314351
Pablo <pablo.perez.colls@gmail.com>
2023-09-11 13:47:00 +0200

    pkm search finished

--- Pokedex/Pokedex.js
+++ Pokedex/Pokedex.js
@@ -1,4 +1,4 @@
-var colors = {
+    var colors = {
     "normal": "rgb(217, 203, 186)",
     "fighting": "rgb(243, 213, 188)",
     "flying": "rgb(192, 218, 229)",
@@ -20,6 +20,30 @@ var colors = {
     "unknown": "rgb(160, 160, 160)",
     "shadow": "rgb(85, 85, 85)"
   };
+  var darkerColors = {
+    "normal": "rgb(120, 110, 95)",
+    "fighting": "rgb(150, 120, 100)",
+    "flying": "rgb(100, 130, 140)",
+    "poison": "rgb(95, 96, 110)",
+    "ground": "rgb(140, 110, 70)",
+    "rock": "rgb(110, 106, 95)",
+    "bug": "rgb(100, 115, 85)",
+    "ghost": "rgb(85, 70, 95)",
+    "steel": "rgb(80, 81, 90)",
+    "fire": "rgb(180, 90, 70)",
+    "water": "rgb(70, 110, 120)",
+    "grass": "rgb(85, 120, 85)",
+    "electric": "rgb(140, 100, 70)",
+    "psychic": "rgb(120, 80, 90)",
+    "ice": "rgb(100, 120, 130)",
+    "dragon": "rgb(120, 80, 50)",
+    "dark": "rgb(90, 80, 70)",
+    "fairy": "rgb(150, 110, 130)",
+    "unknown": "rgb(70, 70, 70)",
+    "shadow": "rgb(40, 40, 40)"
+};
+
+
 const srcImgs = "https://pokeres.bastionbot.org/images/pokemon/"; //n.png
 let container = document.getElementById("container");
 let btnMenu = document.getElementById("btnMenu");
@@ -27,7 +51,6 @@ let menu = document.getElementById("menu")
 let menuThings = menu.querySelectorAll("*");
 let menuActive = false
 let cards
-
 menuThings.forEach((thing)=>{
     thing.style.opacity = 0;
 })
@@ -57,8 +80,9 @@ async function getData(num){
 }
 async function newCard(num){
     let data = await getData(num);
-    let element = `<div class="card" style="background-color: ${colors[data.types[0].type.name]}; box-shadow: 0 0 100px ${colors[data.types[0].type.name]};">
-                        <div class="imgCardCont">
+    let element = `<div class="card" style="background-color: ${colors[data.types[0].type.name]}; box-shadow: 0 0 100px ${colors[data.types[0].type.name]};"
+                        data-gen= "${getGen(num)}" data-type= "${data.types[0].type.name}">
+                        <div class="imgCardCont" style="border: solid 3px ${darkerColors[data.types[0].type.name]};">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png" alt="">
                         </div>
                         <div class="pokemonNbrCard">#${num}</div>
@@ -66,6 +90,17 @@ async function newCard(num){
                     </div>`;
     return element
 }
+function getGen(num){
+    if (num <= 151) return 1;
+    else if (num <= 251) return 2;
+    else if (num <= 386) return 3;
+    else if (num <= 493) return 4;
+    else if (num <= 649) return 5;
+    else if (num <= 721) return 6;
+    else if (num <= 807) return 7;
+    else if (num <= 905) return 8;
+}
+
 async function addCards(nums){
     for (let i = 1; i <= nums; i++) {
         let divAct = document.createElement("div")
@@ -76,7 +111,7 @@ async function addCards(nums){
     }
     cards = container.querySelectorAll(".card")
 }
-addCards(151)
+addCards(905)
 
 // Search
 let search = document.getElementById("search");
@@ -85,10 +120,49 @@ search.addEventListener("input", () => {
     cards.forEach((card) => {
         let nbrAct = card.querySelector(".pokemonNameCard").textContent.toLowerCase(); // Convierte el contenido de texto a minúsculas
         if (!nbrAct.includes(str)) {
-            card.style.display = "none"; // Oculta la tarjeta
+            card.style.display = "none";
         } else {
-            card.style.display = "flex"; // Muestra la tarjeta
+            card.style.display = "flex";
         }
     });
 });
 
+//Menu
+//Generation selection
+let gensCheckbx = document.getElementById("gensCheckbx")
+let arrCheckbox = gensCheckbx.querySelectorAll("input")
+arrCheckbox.forEach((checkbox, index) => {
+    checkbox.addEventListener("input", () => {
+        if (index == 8) {
+            arrCheckbox.forEach((chx, i) => {
+                if (i != 8) {
+                    if (checkbox.checked) {
+                        chx.checked = true;
+                        changeVisibilityGen(chx,i)
+                    } else {
+                        chx.checked = false;
+                        changeVisibilityGen(chx,i)
+                    }
+                }
+            });
+        } else {
+            changeVisibilityGen(checkbox, index)
+        }
+    });
+});
+function changeVisibilityGen(checkbox, index){
+    let inp = index + 1;
+            if (checkbox.checked) {
+                cards.forEach((card) => {
+                    if (card.getAttribute("data-gen") == inp) {
+                        card.style.display = "flex";
+                    }
+                });
+            } else {
+                cards.forEach((card) => {
+                    if (card.getAttribute("data-gen") == inp) {
+                        card.style.display = "none";
+                    }
+                });
+            }
+}
